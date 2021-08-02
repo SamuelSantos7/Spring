404e8a4f1fa4d3977a4d423e9a83d2541c5059de
SamuelSantos7 <samuel.rocha0706@mail.com>
2021-08-02 15:13:04 -0300

    Atividade Back-end farmacia

---
+++ farmacia/farmacia/src/main/java/com/loja/farmacia/model/Categoria.java
@@ -0,0 +1,95 @@
+package com.loja.farmacia.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String nome;
+
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String tipo;
+
+	@NotNull
+	@Size(min = 5, max = 30)
+	private String estado;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Produto produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+}
---
+++ farmacia/farmacia/src/test/java/com/loja/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
